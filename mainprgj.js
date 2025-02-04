6129847da44398b1f3e7ef4a479ba497c06fad78
Apurva Kirve <116294252+apurvakirve@users.noreply.github.com>
2025-02-04 15:53:41 +0530

    Add files via upload

---
+++ mainprgj.js
@@ -0,0 +1,40 @@
+const url =
+	'https://api.openweathermap.org/data/2.5/weather';
+const apiKey =
+	'7985e46a4685ce8018393671f582501f';
+
+$(document).ready(function () {
+	weatherFn('Vasai');
+});
+
+async function weatherFn(cName) {
+	const temp =
+		`${url}?q=${cName}&appid=${apiKey}&units=metric`;
+	try {
+		const res = await fetch(temp);
+		const data = await res.json();
+		if (res.ok) {
+			weatherShowFn(data);
+		} else {
+			alert('City not found. Please try again.');
+		}
+	} catch (error) {
+		console.error('Error fetching weather data:', error);
+	}
+}
+
+function weatherShowFn(data) {
+	$('#city-name').text(data.name);
+	$('#date').text(moment().
+		format('MMMM Do YYYY, h:mm:ss a'));
+	$('#temperature').
+		html(`${data.main.temp}°C`);
+	$('#description').
+		text(data.weather[0].description);
+	$('#wind-speed').
+		html(`Wind Speed: ${data.wind.speed} m/s`);
+    $('#weather-icon').attr('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`); // Correct URL for the icon
+
+	$('#weather-info').fadeIn();
+}
+
